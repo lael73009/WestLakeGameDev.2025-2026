127b2f348869554bcf9c8bb18dc6c52f7debfc25
ainacteru <gbenawijr@gmail.com>
2025-12-07 19:06:25 -0600

    fixed 2 unity projects

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Move : MonoBehaviour
+{     
+    [SerializeField] private float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 input;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleInputs();
+    }
+
+    void FixedUpdate() {
+        rb.linearVelocity = input * moveSpeed;
+    }
+
+    private void HandleInputs() {
+
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+
+        input.Normalize();
+    }
+}
+ 
